958fe3e22ec6e1002850ac441eef7ae4b2d79e8b
xiaoxuan6 <1527736751@qq.com>
2023-04-07 10:17:02 +0800

    fix: env command

--- app/Application.php
+++ app/Application.php
@@ -1,19 +1,30 @@
 <?php
 
+/*
+ * This file is part of james.xue/search.
+ *
+ * (c) vinhson <15227736751@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ */
 
 namespace App;
 
-
+use Exception;
 use App\Commands\EnvCommand;
 use Symfony\Component\Console\Exception\ExceptionInterface;
 
 class Application
 {
     /**
-     * @throws ExceptionInterface
+     * @throws ExceptionInterface|Exception
      */
     public function __invoke()
     {
-        (new \Symfony\Component\Console\Application())->add(new EnvCommand::class)->run();
+        $app = new \Symfony\Component\Console\Application();
+        $app->add(new EnvCommand());
+        $app->run();
     }
 }
--- app/Commands/EnvCommand.php
+++ app/Commands/EnvCommand.php
@@ -1,12 +1,22 @@
 <?php
 
+/*
+ * This file is part of james.xue/search.
+ *
+ * (c) vinhson <15227736751@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ */
 
 namespace App\Commands;
 
-
+use Symfony\Component\Finder\Finder;
+use Vinhson\Search\Commands\Command;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Vinhson\Search\Commands\Command;
 
 class EnvCommand extends Command
 {
@@ -16,8 +26,24 @@ class EnvCommand extends Command
             ->setDescription('设置 .env 配置信息');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        var_export('111');
+        $directory = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "laravel";
+
+        $finder = new Finder();
+        $envFile = iterator_to_array($finder->files()->ignoreDotFiles(false)->in($directory)->name('.env'));
+        $envFileContent = reset($envFile)->getContents();
+
+        $newEnvFile = str_replace(['DB_HOST=127.0.0.1','REDIS_HOST=127.0.0.1'], ['DB_HOST=mysql', 'REDIS_HOST=redis'], $envFileContent);
+
+        $filesystem = new Filesystem();
+        $filesystem->dumpFile($directory . "/.env", $newEnvFile);
+
+        return self::SUCCESS;
     }
 }
