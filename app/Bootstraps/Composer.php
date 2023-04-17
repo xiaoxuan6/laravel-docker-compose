4dc5b2297f7500eeed11da3016d517b60fc2d2ef
xiaoxuan6 <1527736751@qq.com>
2023-04-17 11:35:57 +0800

    fix: check load

--- app/Bootstraps/Composer.php
+++ app/Bootstraps/Composer.php
@@ -12,16 +12,15 @@
 
 namespace App\Bootstraps;
 
-use TitasGailius\Terminal\Terminal;
-use Symfony\Component\Process\ExecutableFinder;
 use Vinhson\Search\Exceptions\RuntimeException;
+use Symfony\Component\Process\{ExecutableFinder, Process};
 
 class Composer
 {
     /**
      * @throws RuntimeException
      */
-    public static function loadMake()
+    public static function load()
     {
         $git = (new ExecutableFinder())->find('git');
 
@@ -29,12 +28,14 @@ class Composer
             throw new RuntimeException("无法获取 git 安装路径");
         }
 
+        $wget = str_replace('git.EXE', 'wget.exe', $git);
+        if (! file_exists($wget)) {
+            throw new RuntimeException("wget 命令不存在，参考地址：https://www.jianshu.com/p/fb6601795011");
+        }
+
         $make = str_replace('git.EXE', 'make.exe', $git);
         if(! file_exists($make)) {
-            $response = Terminal::builder()
-                ->run('search i make');
-
-            $response->output();
+            throw new RuntimeException("make 命令不存在，参考地址：https://github.com/xiaoxuan6/static/releases/download/v1.0.0.beta/make.exe");
         }
     }
 }
